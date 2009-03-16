17307ce34f0ad9472a7ee3a32e84dd8b834e93c9
mario User <mario@ubuntu.ubuntu-domain>
2009-03-16 11:24:42 -0300

    Adicionando arquivo que eu esqueci =P

---
+++ templates/default/lang/pt_BR/lang.ticket.php
@@ -0,0 +1,36 @@
+<?php
+  #
+  # Ticket Header
+  #
+  define('TICKET_HEADER_ID','ID');
+  define('TICKET_HEADER_DATE','Data');
+  define('TICKET_HEADER_STATUS','Status');
+  define('TICKET_HEADER_SUPPORTER','Atendente');
+  define('TICKET_HEADER_ACTIONS','A&ccedil;&otilde;es');
+  
+  #
+  # Ticket History
+  #
+  define('TICKET_HISTORY', 'Hist&oacute;rico');
+  define('MSG_HEAD1','<b>Mensagem enviada em ');
+  define('MSG_HEAD2',' por <span class=\'TxAtendente\'>');
+  define('MSG_HEAD3','</span></b> <br /><br />');
+  define('ATTACHMENT', 'Anexo');
+  
+  #
+  # Ticket Answer
+  #
+  define('ANSWER','Responder');
+  define('MSGTYPE_NORMAL','Mensagem Normal');
+  define('MSGTYPE_INTERNAL','Observa&ccedil;&atilde;o Interna');
+  define('MSGTYPE_SYSTEM','Mensagem do Sistema');
+  define('MSGTYPE_SATISFACTION','Satisfa&ccedil;&atilde;o ao Cliente');
+  
+  #
+  # Buttons
+  #
+  define('ADD','Incluir');
+  define('PREVIEW','Visualizar');
+
+  define('INFORMATIONS','Informa&ccedil;&otilde;es');
+?>
