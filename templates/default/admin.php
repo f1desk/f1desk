7eba044ebcc58079a56d9800662336db1aae162b
John <contato@f1desk.org>
2009-04-07 10:05:26 -0300

    Adicionando a parte de administracao

--- templates/default/admin.php
+++ templates/default/admin.php
@@ -1,9 +1,21 @@
 <?require_once('header.php');?>
 <div id='adminMenu'>
   <ul id='Menu'>
-    <li><a href='javascript:void(0);' onclick="Admin.changeOption('manageUsers.php')">Administrar Usu&aacute;rios</a></li>
-    <li><a href='javascript:void(0);' onclick="Admin.changeOption('manageMenus.php')">Administrar Menus</a></li>
-    <li><a href='javascript:void(0);' onclick="Admin.changeOption('preferences.php')">Prefer&ecirc;ncias</a></li>
+    <li>
+      <a href='javascript:void(0);' onclick="Admin.changeOption('manageUsers.php')">Administrar Usu&aacute;rios</a>
+    </li>
+    <li>
+      <a href='javascript:void(0);' onclick="Admin.changeOption('manageMenus.php')">Administrar Menus</a>
+    </li>
+    <li>
+      <a href='javascript:void(0);' onclick="Admin.changeOption('preferences.php')">Administrar Departamentos</a>
+    </li>
+    <li>
+      <a href='javascript:void(0);' onclick="Admin.changeOption('preferences.php')">Administrar Grupos</a>
+    </li>
+    <li>
+      <a href='javascript:void(0);' onclick="Admin.changeOption('preferences.php')">Prefer&ecirc;ncias</a>
+    </li>
   </ul>
 </div>
 <div id='contentAdminMenu'>
