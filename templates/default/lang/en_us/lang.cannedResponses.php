02f4194b9bf90283ae406622a06cdcf5d9e2031a
mario User <mario@ubuntu.ubuntu-domain>
2009-04-27 11:20:47 -0300

    Linguagem ingles

---
+++ templates/default/lang/en_us/lang.cannedResponses.php
@@ -0,0 +1,7 @@
+<?php
+  define("CANNED_RESPONSES", "Canned Responses");
+  define("NO_CANNED", "No canned responses.");
+  define("TITLE","Title");
+  define("ACTIONS","Actions");
+  define('CREATE_CANNED_RESPONSE', 'Create canned response');
+?>
