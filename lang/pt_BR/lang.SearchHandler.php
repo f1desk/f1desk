eb7a504175ea34170a8042ae9e6575c25ae0f657
danilo <danilo@kickseed.mz.digirati.com.br>
2009-07-08 17:05:12 -0300

    Alteracoes na classe de buscaII - 08-07-2009

---
+++ lang/pt_BR/lang.SearchHandler.php
@@ -0,0 +1,18 @@
+<?php
+  #
+  # All SearchHandler class messages
+  #
+  define('EXC_INVALID_DTSTART', 'Data inicial "%s" inválida');
+  define('EXC_INVALID_IDSUPPORTER', 'IDSuporrter "%s" inválido');
+  define('EXC_INVALID_DTEND','Data final "%s" inválida');
+  define('EXC_INVALID_IDDEPARTMENT', 'IDDepartment "%s" inválido');
+  define('EXC_INVALID_BIGGEST_DT', 'Argumento "%s" deve ser menor que a data atual.');  
+  define('EXC_BAD_ARGUMENT', 'Argumento inválido' ); 
+  define('EXC_DTSTART_BIGGER_THAN_DTEND', 'A data final deve ser menor que data inicial' ); 
+  define('EXC_NUMBER_SHOULD_BE_BIGGER_THAN_ZERO', 'Número deve ser maior que zero.' ); 
+  define('EXC_CANNOT_GROUP', 'Não foi possível agrupar para executar a instrução SQL' ); 
+  define('EXC_INVALID_LIMIT', 'Limite inválido para executar a instrução SQL.');
+  define('EXC_INVALID_PAGE', 'Paginação inválida para executar a instrução SQL.');
+  define('EXC_TABLE_NOT_AVAILABLE', 'Não é permitido ordenar ou agrupar pela tabela "%s".');
+  
+?>
